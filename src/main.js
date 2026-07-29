527de129a5113848b2b416ddfb80338a8e5eda3c
razorbladee <104161605+razorbladee@users.noreply.github.com>
2026-07-29 13:47:03 +0300

    feat: register dense reference visualizers

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './core/experimental-visualizers.js';
 import './core/neon-visualizers.js';
 import './core/future-visualizers.js';
 import './core/reference-visualizers.js';
+import './core/reference-detail-visualizers.js';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './style.css';

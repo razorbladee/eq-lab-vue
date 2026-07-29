2eebb93e2fae8740e629f79ce3bb0c164bc530f8
razorbladee <104161605+razorbladee@users.noreply.github.com>
2026-07-29 13:28:52 +0300

    feat: register future visualizers

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './core/experimental-visualizers.js';
 import './core/neon-visualizers.js';
+import './core/future-visualizers.js';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './style.css';

d5d9b62c53cce0b9efe334854d3b7618583ac056
razorbladee <104161605+razorbladee@users.noreply.github.com>
2026-07-29 13:21:03 +0300

    feat: register neon visualizers

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
+import './core/experimental-visualizers.js';
+import './core/neon-visualizers.js';
 import { createApp } from 'vue';
 import App from './App.vue';
-import './core/experimental-visualizers.js';
 import './style.css';
 
 createApp(App).mount('#app');

8cd4911119292b02f49fe2a78026ed9e346b9d66
razorbladee <104161605+razorbladee@users.noreply.github.com>
2026-07-29 13:37:00 +0300

    feat: register reference visualizers

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './core/experimental-visualizers.js';
 import './core/neon-visualizers.js';
 import './core/future-visualizers.js';
+import './core/reference-visualizers.js';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './style.css';

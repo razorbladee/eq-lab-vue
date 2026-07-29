2dee99c0fa4e70de7353aec6bb4f54b0921025d7
razorbladee <104161605+razorbladee@users.noreply.github.com>
2026-07-29 13:58:19 +0300

    feat: register spectrum lab visualizers

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './core/neon-visualizers.js';
 import './core/future-visualizers.js';
 import './core/reference-visualizers.js';
 import './core/reference-detail-visualizers.js';
+import './core/spectrum-lab.js';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './style.css';
